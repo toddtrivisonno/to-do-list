66026d48efa2b47958db1e5fd4542d3df73cef18
Todd Trivisonno <todd.trivisonno@gmail.com>
2019-10-12 17:38:38 -0400

    list item layout

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
 
+/*--- Create List Item ---*/
 
-/* Create UI */
+function addListItem() {
+   console.log('button clicked');
+
+   let submission = document.getElementById('userInput').value;
+
+   if (submission) {
+
+      // localStorage.setItem(itemNumber, submission);
+
+      let listItem = document.createElement('li');
+      listItem.className = "list-group-item";
+      // listItem.innerHTML = submission;
+      let customCheckbox = document.createElement('div');
+      customCheckbox.className = "input-group";
+      let checkGroup = document.createElement('div');
+      checkGroup.className = "input-group-text";
+      let checkboxInput = document.createElement('input');
+      checkboxInput.setAttribute('type', 'checkbox');
+      let checkboxLabel = document.createElement('div');
+      checkboxLabel.className = "form-control";
+      checkboxLabel.innerHTML = submission;
+
+      checkGroup.appendChild(checkboxInput);
+      customCheckbox.appendChild(checkGroup);
+      customCheckbox.appendChild(checkboxLabel);
+      listItem.appendChild(customCheckbox);
+      flushList.appendChild(listItem);
+   }
+}
+
+
+
+/*--- Create UI ---*/
 
 let outerContainer = document.createElement('div');
 outerContainer.className = "container mx-auto";
 
 let title = document.createElement('p');
-title.className = "h2 text-center mt-3";
+title.className = "h2 text-center text-light mt-3";
 title.innerHTML = "TuDu";
 
 /* Create the Input From and Submit Button */
 let inputItem = document.createElement('div');
 inputItem.className = "input-group";
 let inp = document.createElement("input");
+inp.id = "userInput";
 inp.setAttribute("type", "text");
 inp.setAttribute("placeholder", "Enter a to do...");
 inp.className = "form-control";
@@ -21,6 +55,13 @@ inputGroupAppend.className = "input-group-append";
 let inputBtn = document.createElement('button');
 inputBtn.className = "btn btn-primary";
 inputBtn.innerHTML = "Add Item";
+
+inp.addEventListener('keydown', function (e) {
+   if (e.keyCode === 13) {
+      addListItem();
+   }
+});
+inputBtn.addEventListener('click', addListItem);
 
 inputItem.appendChild(inp);
 inputItem.appendChild(inputGroupAppend);
@@ -64,9 +105,13 @@ tabs.appendChild(allBtn);
 tabs.appendChild(activeBtn);
 tabs.appendChild(completeBtn);
 
-
+/* Create Unordered Flush List */
+let flushList = document.createElement('ul');
+flushList.className = "list-group list-group-flush";
 
 outerContainer.appendChild(title);
 outerContainer.appendChild(inputItem);
 outerContainer.appendChild(tabs);
+outerContainer.appendChild(flushList);
+
 app.appendChild(outerContainer);
